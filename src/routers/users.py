dd028e98b76cba593558e4825b5193a054a7eaa4
Krtlli <krtlli435@gmail.com>
2026-06-10 16:20:53 +0300

    Add user registration by admin (users.py /api/users/add)

--- src/routers/users.py
+++ src/routers/users.py
@@ -1,5 +1,6 @@
 import sqlite3
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, Header
+from schemas import UserAuth
 from typing import List
 from config import DB_FILE
 
@@ -12,3 +13,24 @@ def get_users():
         cursor = conn.cursor()
         cursor.execute("SELECT username FROM users WHERE username != 'admin'")
         return [row[0] for row in cursor.fetchall()]
+    
+@router.post("/api/users/add")
+def admin_register(user: UserAuth, x_token: str = Header(...)):
+    with sqlite3.connect(DB_FILE) as conn:
+        cursor = conn.cursor()
+        
+        cursor.execute("SELECT is_admin FROM users WHERE username = ?", (x_token,))
+        user_row = cursor.fetchone()
+        is_admin = user_row[0] if user_row else 0
+        
+        if not is_admin:
+            raise HTTPException(status_code=403, detail="Доступ запрещен. Только для администраторов")
+
+        cursor.execute("SELECT 1 FROM users WHERE username = ?", (user.username,))
+        if cursor.fetchone():
+            raise HTTPException(status_code=400, detail="Пользователь уже существует")
+
+        cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (user.username, user.password))
+        conn.commit()
+        
+        return {"message": f"Пользователь {user.username} успешно зарегистрирован администратором"}
